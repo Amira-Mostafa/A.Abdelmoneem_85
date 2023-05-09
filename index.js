2b24f7b9b42cb35123921a19f046096ab2badcc4
Amira-Mostafa <111038671+Amira-Mostafa@users.noreply.github.com>
2023-05-09 22:46:29 +0300

    index.js

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
-var x = document.querySelectorAll(".love");
 
-        x.forEach(e => {
-        e.addEventListener("click", ()=> {
-        e.classList.toggle("active") 
-        })
-        });
-        const payment = document.querySelector(".payment");
-const close = document.querySelector(".close");
-
-payment.addEventListener("click", () => {
-  close.style.display = "flex";
-
-});
